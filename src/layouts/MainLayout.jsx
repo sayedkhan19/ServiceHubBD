47cd764fa056f946a1e7fd463b98f50ab25de90a
sayedkhan196 <sayedkhan196>
2025-06-10 11:05:28 +0600

    first commit

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import NavBar from './NavBar';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/pages/BookedServices.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookedServices = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BookedServices;
