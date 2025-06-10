5ae18c90262819fe35a86fb3552d096c07baecbd
sayedkhan196 <sayedkhan196>
2025-06-10 14:24:19 +0600

    add errorPage

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import errorImg from '../assets/download.png';
+import { Link } from 'react-router';
 
 const NotFound = () => {
     return (
-        <div>
-            
-        </div>
+         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 px-4 py-10 text-center">
+      {/* Image Placeholder */}
+      <div className="w-full max-w-md mb-8">
+        <img
+          src={errorImg}
+          alt="Error Illustration"
+          className="w-full h-auto object-contain rounded-xl shadow-lg"
+        />
+      </div>
+
+      <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4">Oops! Page not found</h1>
+      <p className="text-gray-600 mb-6">
+        The page you're looking for doesn't exist or has been moved.
+      </p>
+
+      <Link
+        to="/"
+        className="inline-block px-6 py-3 bg-blue-600 text-white font-medium rounded-2xl shadow hover:bg-blue-700 transition"
+      >
+        Go Home
+      </Link>
+    </div>
     );
 };
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../layouts/MainLayout";
+import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
 
 
 
@@ -7,5 +9,12 @@ export const router = createBrowserRouter([
   {
     path: "/",
     Component:MainLayout,
+    errorElement:<NotFound></NotFound>,
+    children:[
+        {
+            index:true,
+            Component: Home,
+        }
+    ]
   },
 ]);
